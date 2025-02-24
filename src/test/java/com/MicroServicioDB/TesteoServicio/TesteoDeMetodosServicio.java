9f2b8a97309cd3cd51d03c9f65fb6dfeaf375792
chenaoh <migue27_@hotmail.com>
2025-02-24 11:06:54 -0600

    Carpeta de pruebas unitarias creada

---
+++ src/test/java/com/MicroServicioDB/TesteoServicio/TesteoDeMetodosServicio.java
@@ -0,0 +1,11 @@
+package com.MicroServicioDB.BibliotecaDePruebasUnitarias;
+
+public class TestClases {
+    
+    //AQUI ES DONDE VAMOS A REALIZAR LAS PRUEBAS UNITARIAS
+
+    public static void main(String[] args) {
+        System.out.println("Hola pruebas unitarias");
+    }
+
+}
